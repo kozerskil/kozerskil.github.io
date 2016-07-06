91925234a2784392fe9da350c8647bf7b0be7672
Lukasz Kozerski <lookasbox@gmail.com>
2016-07-06 12:13:05 +0200

    ok

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,6 @@
-git fetch & touch kurczak.appcache & git add . & git ci -m "ok" & git push
+echo {"date":"%date% %time%"} > version
+git fetch
+touch kurczak.appcache
+git add .
+git ci -m "ok"
+git push
