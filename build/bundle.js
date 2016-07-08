8c823f851e8e55c36a58e84227172d377d6d63af
Lukasz Kozerski <lookasbox@gmail.com>
2016-07-08 10:24:34 +0200

    ok

--- build/bundle.js
+++ build/bundle.js
@@ -15885,6 +15885,7 @@ $__System.register("11", ["3", "10", "c", "12", "13"], function(exports_1, conte
         }
         LessonsScreenComponent.prototype.ngOnInit = function() {
           var _this = this;
+          this.update = applicationCache.status === applicationCache.UPDATEREADY;
           this.subscriptions.push(this.service.getData().subscribe(function(data) {
             return data.forEach(function(x, k) {
               return setTimeout(function() {
@@ -15909,13 +15910,24 @@ $__System.register("11", ["3", "10", "c", "12", "13"], function(exports_1, conte
           });
         };
         LessonsScreenComponent.prototype.cacheUpdate = function() {
+          var _this = this;
+          if (applicationCache.status === applicationCache.UNCACHED) {
+            return;
+          }
+          if (applicationCache.status === applicationCache.UPDATEREADY) {
+            window.location.pathname = '/';
+            return;
+          }
           applicationCache.addEventListener('updateready', function() {
             return window.location.pathname = '/';
+          }, false);
+          applicationCache.addEventListener('downloading', function() {
+            return _this.update = true;
           }, false);
           applicationCache.update();
         };
         LessonsScreenComponent = __decorate([core_1.Component({
-          template: "\n<div class=\"ui grid container\">\n  <div class=\"twelve wide computer sixteen wide tablet column\">\n    <div class=\"ui three column stackable grid\">\n      <div class=\"column\" *ngFor='let lesson of lessons'>\n        <lesson-card [lesson]='lesson' @animation=\"show\" draggable=\"true\"></lesson-card>\n      </div>\n      <div class=\"center aligned column\">\n        <div class=\"ui button\" (click)=\"cacheUpdate()\">\n            <i class=\"history icon\"></i> {{version}}\n        </div>\n      </div>\n    </div>\n  </div>\n  <div class=\"four wide computer only column\">\n    <div class=\"ui grid\">\n      <div class=\"centered row\">\n        <img class=\"ui image\" src=\"assets/chicken.svg\">\n      </div>\n    </div>\n  </div>\n</div>\n    ",
+          template: "\n<div class=\"ui grid container\">\n  <div class=\"twelve wide computer sixteen wide tablet column\">\n    <div class=\"ui three column stackable grid\">\n      <div class=\"column\" *ngFor='let lesson of lessons'>\n        <lesson-card [lesson]='lesson' @animation=\"show\" draggable=\"true\"></lesson-card>\n      </div>\n      <div class=\"center aligned column\">\n        <div class=\"ui button\" (click)=\"cacheUpdate()\" [class.orange]=\"update\">\n          <i class=\"history icon\"></i> {{version}}\n        </div>\n      </div>\n    </div>\n  </div>\n  <div class=\"four wide computer only column\">\n    <div class=\"ui grid\">\n      <div class=\"centered row\">\n        <img class=\"ui image\" src=\"assets/chicken.svg\">\n      </div>\n    </div>\n  </div>\n</div>\n    ",
           styles: [':host { display: block; }'],
           directives: [lesson_card_component_1.LessonCardComponent],
           animations: [core_1.trigger('animation', [core_1.state('show', core_1.style({opacity: 1})), core_1.transition('void => *', [core_1.style({opacity: 0}), core_1.animate('0.1s ease-in-out')])])]
